48c53d4b87293a5dc23b5b621f83e92fdeff3bd1
Turker Teke <trkrtk@gmail.com>
2024-10-15 02:22:52 +0300

    fix typo in fugitive config

--- lua/tteke/remap.lua
+++ lua/tteke/remap.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>c", ":bd<CR>")
 vim.keymap.set("n", "==", "gg=G")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
